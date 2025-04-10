c6ef015772dcc12f05aa625df182657e2e619f04
balazsJagerWikifolio <balazs.jager@wikifolio.com>
2025-04-10 19:18:54 +0200

    12 - add validations

--- Blog in Blazor/Blog.BlazorServer/ViewModels/Auth/RegisterViewModel.cs
+++ Blog in Blazor/Blog.BlazorServer/ViewModels/Auth/RegisterViewModel.cs
@@ -4,17 +4,26 @@ namespace Blog.BlazorServer.ViewModels.Auth
 {
     public class RegisterViewModel
     {
+        [Display(Name = "Username")]
+        [Required(ErrorMessage = "Username is required")]
+        [StringLength(15, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 2)]
         public string Username { get; set; }
 
         [Display(Name = "Email")]
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid email address")]
         public string Email { get; set; }
 
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [Required(ErrorMessage = "Password is required")]
         public string Password { get; set; }
 
         [DataType(DataType.Password)]
         [Display(Name = "Confirm password")]
+        [Required(ErrorMessage = "Confirm password is required")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
     }
 }
